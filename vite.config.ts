78771a5377a7c57bc60358d858255b94c5590e0c
eaipepeu <80593059+eaipepeu@users.noreply.github.com>
2025-09-04 09:17:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/mobilis-data-vista-32/', // Adicione esta linha
+  plugins: [react()],
+})
+
+// https://vitejs.dev/config/
+export default defineConfig(({ base: '/mobilis-data-vista-32/', }) => ({
   server: {
     host: "::",
     port: 8080,
